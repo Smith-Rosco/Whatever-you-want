6d0ff2ba203bb782253a6d1e5d1e69b81a7c93ea
He Kaifeng <3062787402@qq.com>
2025-05-17 00:06:46 +0800

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -6,13 +6,6 @@ const loadScript = (src) => {
   document.head.appendChild(s);
 };
 
-const loadCSS = (href) => {
-  const l = document.createElement('link');
-  l.rel = 'stylesheet';
-  l.href = href;
-  document.head.appendChild(l);
-};
-
 loadScript('https://cdn.jsdelivr.net/gh/Smith-Rosco/Whatever-you-want/auto_complete.js');
 loadScript('https://cdn.jsdelivr.net/gh/Smith-Rosco/Whatever-you-want/simplify_v2.0.js');
-loadCSS('https://cdn.jsdelivr.net/gh/Smith-Rosco/Whatever-you-want/style.css');
+loadScript('https://cdn.jsdelivr.net/gh/Smith-Rosco/Whatever-you-want/input_script_v2.1.js');
