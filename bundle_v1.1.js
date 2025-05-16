c9629f6976bc0381e56ece4cec5cbfde3c3b94f6
He Kaifeng <3062787402@qq.com>
2025-05-17 00:11:50 +0800

    Update and rename bundle_v1.js to bundle_v1.1.js

--- bundle_v1.1.js
+++ bundle_v1.1.js
@@ -8,4 +8,4 @@ const loadScript = (src) => {
 
 loadScript('https://cdn.jsdelivr.net/gh/Smith-Rosco/Whatever-you-want/auto_complete.js');
 loadScript('https://cdn.jsdelivr.net/gh/Smith-Rosco/Whatever-you-want/simplify_v2.0.js');
-loadScript('https://cdn.jsdelivr.net/gh/Smith-Rosco/Whatever-you-want/input_script_v2.1.js');
+loadScript('https://cdn.jsdelivr.net/gh/Smith-Rosco/Whatever-you-want/input_script_v2.2.js');
