b4eb14d56ccb4106b4234161dc3d3d9a39cb0016
He Kaifeng <3062787402@qq.com>
2025-05-17 12:39:26 +0800

    Update and rename bundle_v1.1.js to bundle_v1.2.js

--- bundle_v1.2.js
+++ bundle_v1.2.js
@@ -5,7 +5,6 @@ const loadScript = (src) => {
   s.async = true;
   document.head.appendChild(s);
 };
-
-loadScript('https://cdn.jsdelivr.net/gh/Smith-Rosco/Whatever-you-want/auto_complete.js');
+loadScript('https://cdn.jsdelivr.net/gh/Smith-Rosco/Whatever-you-want/StatefulChatInputWithOptions.js');
 loadScript('https://cdn.jsdelivr.net/gh/Smith-Rosco/Whatever-you-want/simplify_v2.0.js');
-loadScript('https://cdn.jsdelivr.net/gh/Smith-Rosco/Whatever-you-want/input_script_v2.2.js');
+
