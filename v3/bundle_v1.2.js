918530af4fa3f1b7596af850469fb84f88473756
milo <3062787402@qq.com>
2025-05-24 19:05:12 +0800

    3.1.2

--- v3/bundle_v1.2.js
+++ v3/bundle_v1.2.js
@@ -14,5 +14,5 @@ const loadStylesheet = (href) => {
 };
 
 
-loadStylesheet('https://cdn.jsdelivr.net/gh/Smith-Rosco/Whatever-you-want/v3/style_v1.1.css');
+loadStylesheet('https://cdn.jsdelivr.net/gh/Smith-Rosco/Whatever-you-want/v3/style_v1.2.css');
 loadScript('https://cdn.jsdelivr.net/gh/Smith-Rosco/Whatever-you-want/v3/script.js');
