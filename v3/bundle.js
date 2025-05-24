00ca114cb8aac2db6b0da061b7b639c6919f5d3b
milo <3062787402@qq.com>
2025-05-24 18:00:27 +0800

    v3

---
+++ v3/bundle.js
@@ -0,0 +1,18 @@
+// bundle.js
+const loadScript = (src) => {
+  const s = document.createElement('script');
+  s.src = src;
+  s.async = true;
+  document.head.appendChild(s);
+};
+
+const loadStylesheet = (href) => {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = href;
+    document.head.appendChild(link);
+};
+
+
+loadStylesheet('https://cdn.jsdelivr.net/gh/Smith-Rosco/Whatever-you-want/v3/style.css');
+// loadScript('https://cdn.jsdelivr.net/gh/Smith-Rosco/Whatever-you-want/StatefulChatInputWithOptions_v1.2.js');
