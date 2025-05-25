05aad6838678386dac5a5942d3ff777651c447ab
milo <3062787402@qq.com>
2025-05-25 17:10:39 +0800

    3.1.3

--- v3.1/bundle_v1.3.js
+++ v3.1/bundle_v1.3.js
@@ -18,4 +18,4 @@ loadStylesheet('https://cdn.jsdelivr.net/gh/Smith-Rosco/Whatever-you-want/v3.1/s
 loadScript('https://cdn.jsdelivr.net/gh/Smith-Rosco/Whatever-you-want/v3.1/DOMWatcherService.js');
 loadScript('https://cdn.jsdelivr.net/gh/Smith-Rosco/Whatever-you-want/v3.1/input_v1.1.js');
 loadScript('https://cdn.jsdelivr.net/gh/Smith-Rosco/Whatever-you-want/v3.1/simplify_v1.1.js');
-loadScript('https://cdn.jsdelivr.net/gh/Smith-Rosco/Whatever-you-want/v3.1/sidebar_v1.1.js');
+loadScript('https://cdn.jsdelivr.net/gh/Smith-Rosco/Whatever-you-want/v3.1/sidebar_v1.2.js');
